a59d6c3d084dbf58b81aee649b76fa30091b657f
Chris Chan <chrisstudio1016@gmail.com>
2024-09-05 18:02:29 +0800

    feat: add more destinations

--- src/constants/destinations.ts
+++ src/constants/destinations.ts
@@ -4,4 +4,6 @@ export enum TravelDestinations {
   Okinawa = 'tfs=CBwQARoXagwIAhIIL20vMDNoNjRyBwgBEgNPS0EaF2oHCAESA09LQXIMCAISCC9tLzAzaDY0QAFIAXABggELCP___________wGYAQE&tfu=KgIIAw&hl=en&gl=HK',
 
   Fukuoka = 'tfs=CBwQARoXagwIAhIIL20vMDNoNjRyBwgBEgNGVUsaF2oHCAESA0ZVS3IMCAISCC9tLzAzaDY0QAFIAXABggELCP___________wGYAQE&tfu=KgIIAw&hl=en&gl=HK',
+
+  Osaka = 'tfs=CBwQARoXagwIAhIIL20vMDNoNjRyBwgBEgNLSVgaF2oHCAESA0tJWHIMCAISCC9tLzAzaDY0QAFIAXABggELCP___________wGYAQE&tfu=KgIIAw',
 }
